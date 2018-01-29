e615d385ff7161377327f7ac0bed9020bbfc90f1
agatito <agatito@yopmail.com>
2018-01-29 19:02:08 +0100

    dziedziczenie

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+    Employee empl =new Employee("Jan", "Kowalski", "Wroc", 2030);
+    Customer cust=new Customer("Tadeusz", "Nowak", "Warszawa", 4500);
+
+    empl.showInfo();
+    System.out.println();
+    cust.showInfo();
+
+
+
+
+    }
+
+
+}
